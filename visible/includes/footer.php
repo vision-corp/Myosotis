45f46e020784485350d2c0b004c153656444a2b0
Florian Hyver <florian.hyver@iut-rodez.fr>
2019-11-21 16:34:43 +0100

    pour que jc commence presentation et contact

---
+++ visible/includes/footer.php
@@ -0,0 +1,9 @@
+<!-- FOOTER -->
+<footer class="container">
+    <p class="float-right"><a href="/visible/visible.php">Retour en haut</a></p>
+    <p>Les Myosotis · <a href="/visible/visible.php">Mentions légales</a> 
+  </footer>
+</main>
+<script src="../js/jquery.js"></script>
+      <script>window.jQuery || document.write('<script src="../js/jquery.js"><\/script>')</script><script src="../js/bootstrap.js"></script>
+</body></html>
---
+++ visible/includes/header.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<!-- saved from url=(0053)https://getbootstrap.com/docs/4.3/examples/carousel/# -->
+<html lang="fr"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Florian Hyver, Tom Margalejo, J-Charles Luans, Tanguy Fenouillot, and Bootstrap contributors">
+    <title>Les Myosotis</title>
+
+    <!-- Bootstrap core CSS -->
+<link href="../css/bootstrap.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="../css/visible.css" rel="stylesheet">
+  </head>
+  <body style="">
+    <header>
+  <!-- Menu sidebar -->
+  <?php require '_nav.php'; ?>
+</header>
+
+<main role="main">
+
+    
